d39f1caf9c1e05404f1f977976d47c49d28dba1d
Ghawrii <zaeemhafizmuhammad@gmail.com>
2023-12-15 10:06:43 +0500

    Leaving this as it is and jumping onto The Complete JavaScript Course

---
+++ STL/container-nesting.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<pair<int, string>> v;
+    v.push_back({1, "v"});
+    cout << v[0].first << " " << v[0].second << endl;
+
+    return 0;
+}
